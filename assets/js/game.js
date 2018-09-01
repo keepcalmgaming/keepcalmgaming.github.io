ea9ef6f18dd94a3745b0befbc2e569beecaf33a0
Misha Serdyuk <thePooh.12@gmail.com>
2018-09-01 20:15:11 +0300

    Adds hook

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,20 +19,39 @@ var config = {
 var game = new Phaser.Game(config);
 var platforms;
 var ball;
+var hook;
 
 function preload ()
 {
   this.load.image('ball', 'assets/images/ball.png');
+  this.load.image('hook', 'assets/images/hook.png');
 }
 
 function create ()
 {
-  this.cameras.main.setBackgroundColor('#ffffff');
+  this.cameras.main.setBackgroundColor('#44BBA4');
   platforms = this.physics.add.staticGroup();
   ball = this.physics.add.sprite(300, 500, 'ball');
   ball.setCollideWorldBounds(true);
+
+  hook = this.physics.add.sprite(0, 0, 'hook');
+  hook.setScale(0.3);
+  hook.body.allowGravity = false;
+  hook.setActive(false);
+  hook.setVisible(false);
 }
 
 function update ()
 {
+    this.input.on('pointerdown', function (pointer) {
+        hook.setActive(true);
+        hook.setVisible(true);
+        hook.x = pointer.x;
+        hook.y = pointer.y;
+    }, this);
+
+    this.input.on('pointerup', function(pointer) {
+        hook.setActive(false);
+        hook.setVisible(false);
+    })
 }
