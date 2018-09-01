35c6b8560cdeb64c5d5ce247585df95091686979
Misha Serdyuk <thePooh.12@gmail.com>
2018-09-01 19:39:23 +0300

    Some physics

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,24 +1,36 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: window.innerWidth,
+    height: window.innerHeight,
     scene: {
         preload: preload,
         create: create,
         update: update
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: true
+        }
     }
 };
 
 var game = new Phaser.Game(config);
+var platforms;
+var ball;
 
 function preload ()
 {
-  this.load.spritesheet('ball', 'assets/images/ball.png', { frameWidth: 32, frameHeight: 48 });
+  this.load.image('ball', 'assets/images/ball.png');
 }
 
 function create ()
 {
   this.cameras.main.setBackgroundColor('#ffffff');
+  platforms = this.physics.add.staticGroup();
+  ball = this.physics.add.sprite(300, 500, 'ball');
+  ball.setCollideWorldBounds(true);
 }
 
 function update ()
