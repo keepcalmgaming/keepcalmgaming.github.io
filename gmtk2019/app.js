a04c674ea04d76ddd1f7a731d99506d671a996c7
Misha Serdyuk <thePooh.12@gmail.com>
2019-08-04 21:53:03 +0300

    Final polish

--- gmtk2019/app.js
+++ gmtk2019/app.js
@@ -8,21 +8,37 @@ define("scenes/greeting", ["require", "exports"], function (require, exports) {
     class GreetingScene extends Phaser.Scene {
         create() {
             var content = [
-                "A Hero, A Sword and A Pit",
+                "B#",
                 "",
-                "This is GMTK Game Jam 2019 submission.",
-                "Only one.",
+                "Topic of GMTK Game Jam 2019 is “only one”.",
+                "We decided to make a tower defense game with only one tower.",
+                "Instead of building new towers, you should change location of your only tower.",
                 "",
-                "Have fun and thanks for playing!",
+                "Other features of the game:",
+                "– minimalistic design inspired of the lack of drawing skills;",
+                "– monsters are evil and you can kill them;",
+                "– soundtrack uses only one note – C.",
+                "We decided C is a boring name, so we called our game B#;",
+                "– no lootboxes;",
+                "– no microtransactions. ",
                 "",
+                "We hope you’ll have as much fun as we did while developing B#.",
+                "",
+                "Enjoy!",
+                "",
+                "(click to play, load takes a while)",
                 "https://keepcalmgaming.github.io"
             ];
             var text = this.add.text(0, 0, content, { align: 'center' });
             var bounds = text.getBounds();
             text.x = halfWidth - bounds.width / 2;
             text.y = halfHeight - bounds.height / 2;
+            let clicked = false;
             this.input.on('pointerdown', () => {
-                this.scene.switch('main');
+                if (!clicked) {
+                    this.scene.switch('main');
+                    clicked = true;
+                }
             });
         }
     }
--- gmtk2019/src/scenes/greeting.ts
+++ gmtk2019/src/scenes/greeting.ts
@@ -7,13 +7,25 @@ const halfWidth = gameWidth / 2
 export class GreetingScene extends Phaser.Scene {
     create() {
         var content = [
-            "A Hero, A Sword and A Pit",
+            "B#",
             "",
-            "This is GMTK Game Jam 2019 submission.",
-            "Only one.",
+            "Topic of GMTK Game Jam 2019 is “only one”.",
+            "We decided to make a tower defense game with only one tower.",
+            "Instead of building new towers, you should change location of your only tower.",
             "",
-            "Have fun and thanks for playing!",
+            "Other features of the game:",
+            "– minimalistic design inspired of the lack of drawing skills;",
+            "– monsters are evil and you can kill them;",
+            "– soundtrack uses only one note – C.",
+            "We decided C is a boring name, so we called our game B#;",
+            "– no lootboxes;",
+            "– no microtransactions. ",
             "",
+            "We hope you’ll have as much fun as we did while developing B#.",
+            "",
+            "Enjoy!",
+            "",
+            "(click to play, load takes a while)",
             "https://keepcalmgaming.github.io"
         ];
 
@@ -23,8 +35,13 @@ export class GreetingScene extends Phaser.Scene {
         text.x = halfWidth - bounds.width/2;
         text.y = halfHeight - bounds.height/2;
 
+        let clicked = false
+
         this.input.on('pointerdown', () => {
-          this.scene.switch('main');
+            if (!clicked) {
+              this.scene.switch('main');
+              clicked = true
+            }
         });
     }
 }
