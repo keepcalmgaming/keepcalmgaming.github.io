473ff663c924bc22ca4f29b4cabb7a98b137c7ee
Denys Dymko <denis@rademade.com>
2019-08-04 20:22:29 +0300

    Only one path

--- gmtk2019/app.js
+++ gmtk2019/app.js
@@ -44,7 +44,7 @@ define("game/game", ["require", "exports"], function (require, exports) {
         ],
         [
             [2, 1, 0, 0, 0],
-            [0, 1, 0, 0, 0],
+            [0, 1, 1, 1, 1],
             [0, 0, 0, 0, 0],
             [1, 3, 1, 1, 0],
             [1, 1, 0, 0, 0],
